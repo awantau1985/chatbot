35a59303a0f1b488b57719e295e5f64c28f65944
awantau <hermawan.saputra1985@gmail.com>
2025-08-07 15:40:59 +0700

    add message page

--- resources/js/Pages/Messages.jsx
+++ resources/js/Pages/Messages.jsx
@@ -1,13 +1,13 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
-export default function Dashboard({ auth }) {
+export default function Messages({ auth }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Messages</h2>}
         >
-            <Head title="Message" />
+            <Head title="Messages" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
